3c72cc8faa89dc55bf11fb5ec5e065c95a214149
thuynhidanngiu <90028983+thuynhidanngiu@users.noreply.github.com>
2021-09-03 15:05:29 +0700

    Update config.js

--- config.js
+++ config.js
@@ -1,19 +1,20 @@
 const CONFIG = {
-    introTitle: 'Babe à!',
-    introDesc: `Trái đất vốn lạ thường
-    Mà sao em cứ đi nhầm đường
-    Lạc vào tim anh lẻ loi
-    Đằng sau chữ yêu đây là thương`,
-    btnIntro: 'hihi',
-    title: 'Phải chăng em đã yêu ngay từ cái nhìn đầu tiên 😙',
-    desc: 'Phải chăng em đã say ngay từ lúc thấy nụ cười ấy ',
-    btnYes: 'Vẫn cứ là thích anh <33',
-    btnNo: 'Không, Anh trai à :3',
-    question:'Trên thế giới hơn 7 tỉ người mà sao em lại yêu anh <3',
-    btnReply: 'Gửi cho anh <3',
+    introTitle: 'anh iu à!',
+    introDesc: `Hôm nay là ngày sinh nhật của anh
+    Em muốn tạo nhiều điều bất ngờ cho anh nhưng mà vì dịch bệnh nên hong thể ^^
+    😘 anh cố gắng nha, hết dịch mình lại được ở bên nhau :3
+    Cảm ơn annh vì đến bên em và cho em hạnh phúc',
+    btnIntro: 'yêu anh ❤',
+    title: ' Chúc anh sinh nhật thật vui vẻ và hạnh phúc 🥰',
+    desc: 'tuổi mới bae xinh trai hơn, yêu bé nhiều nhiều hơn nữa nha 😘 ',
+    desc: 'anh có iu em nhìu hăm',
+    btnYes: 'thương nhắmm óoo <33',
+    btnNo: 'hong đâuu:3',
+    question:'Trên thế giới hơn 7 tỉ người mà sao anh lại yêu em dọ 🥺 <3',
+    btnReply: 'Gửi cho bé <3',
     reply: 'Yêu thì yêu mà không yêu thì yêu <33333333',
-    mess: 'Anh biết mà 🥰. Yêu em nhiều nhiều 😘😘',
-    messDesc: 'Tối nay 7h anh qua đón nhé công chúa.',
+    mess: 'bé biết mà 🥰. Yêu anh nhiều nhiều 😘😘',
+    messDesc: 'khi nào hết dịch em sẽ bù đắp lại cho anh nhá bae :3',
     btnAccept: 'Okiiiii lun <3',
     messLink: 'http://fb.com' //link mess của các bạn. VD: https://m.me/nam.nodemy
 }
